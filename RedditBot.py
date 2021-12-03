0c5c3868f62453ee5cbb73f90b2ccca5fe78add3
Madelyn Spalding <mspaldingworks@gmail.com>
2021-12-03 12:35:32 -0500

    Initial commit. scored client_id and client_secret

---
+++ RedditBot.py
@@ -0,0 +1,11 @@
+import praw
+
+import praw
+
+reddit = praw.Reddit(
+    client_id="QJSd_L86JTCDXhlWDYbPSA",
+    client_secret="	w8bwUejROGLB7k5R9Z2DwEzMnmmjsw",
+    user_agent="<console:HAPPY:1.0>",
+)
+
+subreddit = reddit.subreddit("conspiracy")
